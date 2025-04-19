c9bc448eb3e96c1de2a00f8bac9058dbb520c8e2
Gosvindraj <gosvind@gmail.com>
2025-04-20 00:16:18 +0800

    add typewriter in about page

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+const toggleBtn = document.querySelector(".toggle_btn");
+const toggleBtnIcon = document.querySelector(".toggle_btn i");
+const dropdownMenu = document.querySelector(".dropdown_menu");
 
-    const toggleBtn = document.querySelector('.toggle_btn');
-    const toggleBtnIcon = document.querySelector('.toggle_btn i');
-    const dropdownMenu = document.querySelector('.dropdown_menu');
+toggleBtn.addEventListener("click", function () {
+  dropdownMenu.classList.toggle("open");
+});
 
-    toggleBtn.addEventListener('click', function () {
-        dropdownMenu.classList.toggle('open');
-    });
+//typewrite @about
+const introText =
+  "Hey there! There's not much to say about me, except that I'm really into technology and innovation. I have a strong interest in AI and blockchain. This site is just a little experiment of mine. Nothing too serious, haha.";
+const target = document.getElementById("typewriter");
+let idx = 0;
 
+function typeWriter() {
+  if (idx < introText.length) {
+    target.textContent += introText.charAt(idx++);
+    setTimeout(typeWriter, 50);
+  }
+}
+
+// kick it off once the DOM is ready
+document.addEventListener("DOMContentLoaded", typeWriter);
